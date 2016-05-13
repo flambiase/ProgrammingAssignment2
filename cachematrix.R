0c27b5d603408bb3e03e82e84391ac1d5c614316
flambiase <flambiase@gmail.com>
2016-05-13 06:38:51 -0500

    Updated for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions caches the inverse of a matrix for
+## reuse to avoid multiple costly calculations where possible.
 
-## Write a short comment describing this function
-
+## This function caches a matrix and its inverse for later use.
 makeCacheMatrix <- function(x = matrix()) {
-
+  cmx <<- x
+  cix <<- solve(x)
 }
 
-
-## Write a short comment describing this function
-
+## This function returns the inverse of a matrix. If a cached 
+## version is available, that is passed. If not, or if the 
+## matrix passed differs from the cached version, the inverse is 
+## calculated. Messages are used to indicate which criteria were met.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if (!is.null(cix)){
+    if(identical(x, cmx)){
+      message("getting cached data")
+      return(cix)
+    }
+    message("matrix has changed")
+  }
+  message("calculating inverse")
+  ix <- solve(x)
+  ix
 }
